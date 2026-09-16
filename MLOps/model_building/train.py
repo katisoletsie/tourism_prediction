4916386beabb694d85f32ebf9b3b9e290474c5af
katisoletsie <katisoseeisoletsie@rocketmail.com>
2026-09-16 21:28:00 +0200

    ipynb Commit

--- MLOps/model_building/train.py
+++ MLOps/model_building/train.py
@@ -1,14 +1,25 @@
 # for data manipulation
 import pandas as pd
+
 # for building the preprocessing and modeling pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
+
+# for model training, tuning, and evaluation
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-# for model serialization and experiment tracking
+from sklearn.metrics import accuracy_score, classification_report, recall_score
+
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi, create_repo
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+
+# for model serialization and 
 import joblib
+
+# for experiment tracking
 import mlflow
 
 mlflow.set_tracking_uri("http://localhost:5000")   # complete the code to set the MLflow tracking URI
